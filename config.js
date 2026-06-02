75fb2991e5bb75d7b73792ced4fe4a812045b4b0
Ignacio Borr <145501227+IgnaBorr@users.noreply.github.com>
2026-06-02 14:02:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // Nunca uses sb_secret_ ni service_role.
 window.AGENDA_CONFIG = {
   SUPABASE_URL: "https://dqftsxdivfrrkdlrhqsq.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxZnRzeGRpdmZycmtkbHJocXNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMTY3NzYsImV4cCI6MjA5NTc5Mjc3Nn0.VuTZw5PJ4OiC2T6-2Utpx7ADSGMWQPhhgF8pCtX7bTU"
+  SUPABASE_ANON_KEY: "sb_publishable_OM05vMrKX-EZYKFDSrsLXw_P3nu_lal"
 };
